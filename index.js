98798d5f5eb4c47b12e3117d6cab4fed5ba3049e
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-12 20:12:21 -0400

    commit tree b215ddc2da20ca2b2c3bab00fc73db87b9de4c18

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#text").textContent = "This is really cool!";
+});
